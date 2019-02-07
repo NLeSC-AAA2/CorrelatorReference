d940228bc4fc9bb3123527261b06fe0b5c0b2b9c
Merijn Verstraaten <merijn@inconsistent.nl>
2019-02-07 15:43:08 +0100

    Reorganise functions to keep core implementations close together.

--- pipeline.cpp
+++ pipeline.cpp
@@ -72,124 +72,31 @@ static const bool delay_compensation = DELAY_COMPENSATION;
 static const bool bandpass_correction = BANDPASS_CORRECTION;
 static const bool output_check = true;
 
-////// FIR filter
-
-static InputDataType
-inputTestPattern(bool isFused = false)
-{
-    InputDataType result(InputDataDims);
-
-    if (isFused) {
-        std::fill_n(result.data(), result.num_elements(), 0);
-        if (NR_INPUTS > 6 && NR_SAMPLES_PER_CHANNEL > 27 && NR_CHANNELS > 12) {
-            result[6][27 + NR_TAPS - 1][12] = {2, 3};
-        }
-    } else {
-        const int totalTime = NR_SAMPLES_PER_CHANNEL + NR_TAPS - 1;
-        signed char count = 64;
-        for (unsigned input = 0; input < NR_INPUTS; input ++) {
-            for (unsigned time = 0; time < totalTime; time ++) {
-                for (unsigned channel = 0; channel < NR_CHANNELS; channel ++) {
-                    result[input][time][channel] = std::complex<float>(count, static_cast<signed char>(totalTime * NR_CHANNELS + count));
-                    count++;
-                }
-            }
-
-            count += totalTime * NR_CHANNELS;
-        }
-
-        if (NR_INPUTS > 9 && NR_SAMPLES_PER_CHANNEL > 99 && NR_CHANNELS > 12) {
-            result[9][98 + NR_TAPS - 1][12] = {4, result[9][98 + NR_TAPS - 1][12].imag()};
-            result[9][99 + NR_TAPS - 1][12] = {5, result[9][99 + NR_TAPS - 1][12].imag()};
-        }
-    }
-
-    return result;
-}
-
-static FilterWeightsType
-filterWeightsTestPattern(bool isFused = false)
-{
-    FilterWeightsType result(FilterWeightsDims);
-    std::fill_n(result.data(), result.num_elements(), 0.0f);
-
-    if (isFused && NR_TAPS > 11 && NR_CHANNELS > 12) {
-        result[15][12] = 2;
-    } else if (NR_TAPS > 4 && NR_CHANNELS > 12) {
-        result[15][12] = 2;
-        result[14][12] = 3;
-    }
-
-    return result;
-}
-
-
 static FilteredDataType
-FIR_filter
-( const InputDataType& inputData
-, const FilterWeightsType& filterWeights
-)
-{
-    FilteredDataType filteredData(FilteredDataDims);
-#pragma omp parallel
-    {
-#pragma omp for schedule(dynamic)
-        for (unsigned input = 0; input < NR_INPUTS; input ++) {
-            HistoryType history(HistoryDims);
+FIR_filter(const InputDataType&, const FilterWeightsType&);
 
-            for (unsigned time = 0; time < NR_TAPS - 1; time ++) {
-                for (unsigned channel = 0; channel < NR_CHANNELS; channel ++) {
-                    history[time][channel] = inputData[input][time][channel];
-                }
-            }
-
-            for (unsigned time = 0; time < NR_SAMPLES_PER_CHANNEL; time ++) {
-                for (unsigned channel = 0; channel < NR_CHANNELS; channel ++) {
-                    history[(time - 1) % NR_TAPS][channel] = inputData[input][time + NR_TAPS - 1][channel];
-
-                    filteredData[input][time][channel] = {0, 0};
-
-                    for (unsigned tap = 0; tap < NR_TAPS; tap ++) {
-                        filteredData[input][time][channel] += filterWeights[tap][channel] * history[(time + tap) % NR_TAPS][channel];
-                    }
-                }
-            }
-        }
-    }
-
-    return filteredData;
-}
-
+static CorrectedDataType
+transpose(const FilteredDataType&, const BandPassCorrectionWeights&);
 
 static void
-checkFIR_FilterTestPattern(const FilteredDataType& filteredData)
-{
-    for (unsigned input = 0; input < NR_INPUTS; input ++)
-        for (unsigned time = 0; time < NR_SAMPLES_PER_CHANNEL; time ++)
-            for (unsigned channel = 0; channel < NR_CHANNELS; channel ++)
-                if (filteredData[input][time][channel] != (0.0f + 0.0if)) {
-                    cout << "input = " << input << ", time = " << time
-                         << ", channel = " << channel << ", sample = "
-                         << filteredData[input][time][channel]
-                         << std::endl;
-                }
-}
+applyDelays(CorrectedDataType&, const DelaysType&, const DelaysType&, double);
 
+static CorrectedDataType
+fused
+( const InputDataType&
+, const FilterWeightsType&
+, const BandPassCorrectionWeights&
+, const DelaysType&
+, const DelaysType&
+, double
+);
 
-static FilteredDataType
-testFIR_Filter()
-{
-    const auto& filteredData = FIR_filter(inputTestPattern(), filterWeightsTestPattern());
-
-    if (output_check) checkFIR_FilterTestPattern(filteredData);
-    return filteredData;
-}
-
+static VisibilitiesType
+correlate(const CorrectedDataType&);
 
 ////// FFT
 
 static DFTI_DESCRIPTOR_HANDLE handle;
-
 
 static void
 fftInit()
@@ -260,7 +167,244 @@ FFT(FilteredDataType& filteredData)
 }
 
 
-////// transpose
+////// Initialise data
+
+static InputDataType
+inputTestPattern(bool isFused = false)
+{
+    InputDataType result(InputDataDims);
+
+    if (isFused) {
+        std::fill_n(result.data(), result.num_elements(), 0);
+        if (NR_INPUTS > 6 && NR_SAMPLES_PER_CHANNEL > 27 && NR_CHANNELS > 12) {
+            result[6][27 + NR_TAPS - 1][12] = {2, 3};
+        }
+    } else {
+        const int totalTime = NR_SAMPLES_PER_CHANNEL + NR_TAPS - 1;
+        signed char count = 64;
+        for (unsigned input = 0; input < NR_INPUTS; input ++) {
+            for (unsigned time = 0; time < totalTime; time ++) {
+                for (unsigned channel = 0; channel < NR_CHANNELS; channel ++) {
+                    result[input][time][channel] = std::complex<float>(count, static_cast<signed char>(totalTime * NR_CHANNELS + count));
+                    count++;
+                }
+            }
+
+            count += totalTime * NR_CHANNELS;
+        }
+
+        if (NR_INPUTS > 9 && NR_SAMPLES_PER_CHANNEL > 99 && NR_CHANNELS > 12) {
+            result[9][98 + NR_TAPS - 1][12] = {4, result[9][98 + NR_TAPS - 1][12].imag()};
+            result[9][99 + NR_TAPS - 1][12] = {5, result[9][99 + NR_TAPS - 1][12].imag()};
+        }
+    }
+
+    return result;
+}
+
+static FilterWeightsType
+filterWeightsTestPattern(bool isFused = false)
+{
+    FilterWeightsType result(FilterWeightsDims);
+    std::fill_n(result.data(), result.num_elements(), 0.0f);
+
+    if (isFused && NR_TAPS > 11 && NR_CHANNELS > 12) {
+        result[15][12] = 2;
+    } else if (NR_TAPS > 4 && NR_CHANNELS > 12) {
+        result[15][12] = 2;
+        result[14][12] = 3;
+    }
+
+    return result;
+}
+
+
+static BandPassCorrectionWeights
+bandPassTestPattern(bool isFused = false)
+{
+    BandPassCorrectionWeights result(BandPassCorrectionWeightsDims);
+    std::fill_n(result.data(), result.num_elements(), 1);
+
+    if (!isFused && NR_CHANNELS > 5)
+        result[5] = 2;
+
+    return result;
+}
+
+
+static DelaysType
+delaysTestPattern(bool isBegin, bool isFused = false)
+{
+    DelaysType result(DelaysDims);
+    std::fill_n(result.data(), result.num_elements(), 0);
+
+    if (!isBegin && !isFused && NR_INPUTS > 22)
+        result[22] = 1e-6;
+
+    return result;
+}
+
+
+////// Check results
+
+static void
+checkFIR_FilterTestPattern(const FilteredDataType& filteredData)
+{
+    for (unsigned input = 0; input < NR_INPUTS; input ++)
+        for (unsigned time = 0; time < NR_SAMPLES_PER_CHANNEL; time ++)
+            for (unsigned channel = 0; channel < NR_CHANNELS; channel ++)
+                if (filteredData[input][time][channel] != (0.0f + 0.0if)) {
+                    cout << "input = " << input << ", time = " << time
+                         << ", channel = " << channel << ", sample = "
+                         << filteredData[input][time][channel]
+                         << std::endl;
+                }
+}
+
+
+static void
+checkTransposeTestPattern(const CorrectedDataType& correctedData)
+{
+    for (int channel = 0; channel < NR_CHANNELS; channel ++)
+        for (int time = 0; time < NR_SAMPLES_PER_CHANNEL; time ++)
+            for (int input = 0; input < NR_INPUTS; input ++)
+                if (correctedData[channel][input][time] != (0.0f + 0.0if)) {
+                    cout << "channel = " << channel << ", time = " << time
+                         << ", input = " << input << ", value = "
+                         << correctedData[channel][input][time]
+                         << std::endl;
+                }
+}
+
+
+static void
+checkFusedTestPattern(const CorrectedDataType& correctedData)
+{
+    for (unsigned input = 0; input < NR_INPUTS; input ++)
+        for (unsigned time = 0; time < NR_SAMPLES_PER_CHANNEL; time ++)
+            for (unsigned channel = 0; channel < NR_CHANNELS; channel ++)
+                if (correctedData[channel][input][time] != (0.0f + 0.0if)) {
+                    cout << "input = " << input << ", time = " << time
+                         << ", channel = " << channel << ": ("
+                         << correctedData[channel][input][time].real()
+                         << ", " << correctedData[channel][input][time].imag()
+                         << ')' << std::endl;
+                }
+}
+
+
+static void
+checkCorrelatorTestPattern(const VisibilitiesType& visibilities)
+{
+    for (unsigned channel = 0; channel < NR_CHANNELS; channel ++)
+        for (unsigned baseline = 0; baseline < NR_BASELINES; baseline ++)
+            if (visibilities[channel][baseline] != (0.0f + 0.0if)) {
+                cout << "channel = " << channel << ", baseline = " << baseline
+                     << ", visibility = " << visibilities[channel][baseline]
+                     << std::endl;
+            }
+}
+
+
+////// Test runs
+
+static FilteredDataType
+testFIR_Filter()
+{
+    const auto& filteredData = FIR_filter(inputTestPattern(), filterWeightsTestPattern());
+
+    if (output_check) checkFIR_FilterTestPattern(filteredData);
+    return filteredData;
+}
+
+
+static CorrectedDataType
+testTranspose(FilteredDataType& filteredData)
+{
+    if (NR_INPUTS > 22 && NR_SAMPLES_PER_CHANNEL > 99 && NR_CHANNELS > 5) {
+        filteredData[22][99][5] = {2, 3};
+    }
+
+    BandPassCorrectionWeights bandPassCorrectionWeights(BandPassCorrectionWeightsDims);
+    if (bandpass_correction) {
+        bandPassCorrectionWeights = bandPassTestPattern();
+    }
+
+    auto correctedData = transpose(filteredData, bandPassCorrectionWeights);
+
+    if (delay_compensation) {
+        applyDelays(correctedData, delaysTestPattern(true), delaysTestPattern(false), 60e6);
+    }
+
+    if (output_check) checkTransposeTestPattern(correctedData);
+    return correctedData;
+}
+
+
+static void
+testFused()
+{
+    auto correctedData = fused(
+            inputTestPattern(true), filterWeightsTestPattern(true),
+            bandPassTestPattern(true),
+            delaysTestPattern(true, true), delaysTestPattern(false, true), 60e6);
+
+    if (output_check) checkFusedTestPattern(correctedData);
+}
+
+
+static void
+testCorrelator(CorrectedDataType& correctedData)
+{
+    if constexpr (NR_CHANNELS > 5 && NR_SAMPLES_PER_CHANNEL > 99 && NR_INPUTS > 19) {
+        correctedData[5][ 0][99] = {3,4};
+        correctedData[5][18][99] = {5,6};
+    }
+
+    auto visibilities = correlate(correctedData);
+
+    if (output_check) checkCorrelatorTestPattern(visibilities);
+}
+
+
+////// non-fused
+
+static FilteredDataType
+FIR_filter
+( const InputDataType& inputData
+, const FilterWeightsType& filterWeights
+)
+{
+    FilteredDataType filteredData(FilteredDataDims);
+#pragma omp parallel
+    {
+#pragma omp for schedule(dynamic)
+        for (unsigned input = 0; input < NR_INPUTS; input ++) {
+            HistoryType history(HistoryDims);
+
+            for (unsigned time = 0; time < NR_TAPS - 1; time ++) {
+                for (unsigned channel = 0; channel < NR_CHANNELS; channel ++) {
+                    history[time][channel] = inputData[input][time][channel];
+                }
+            }
+
+            for (unsigned time = 0; time < NR_SAMPLES_PER_CHANNEL; time ++) {
+                for (unsigned channel = 0; channel < NR_CHANNELS; channel ++) {
+                    history[(time - 1) % NR_TAPS][channel] = inputData[input][time + NR_TAPS - 1][channel];
+
+                    filteredData[input][time][channel] = {0, 0};
+
+                    for (unsigned tap = 0; tap < NR_TAPS; tap ++) {
+                        filteredData[input][time][channel] += filterWeights[tap][channel] * history[(time + tap) % NR_TAPS][channel];
+                    }
+                }
+            }
+        }
+    }
+
+    return filteredData;
+}
+
 
 static CorrectedDataType
 transpose
@@ -321,72 +465,6 @@ applyDelays
 }
 
 
-static BandPassCorrectionWeights
-bandPassTestPattern(bool isFused = false)
-{
-    BandPassCorrectionWeights result(BandPassCorrectionWeightsDims);
-    std::fill_n(result.data(), result.num_elements(), 1);
-
-    if (!isFused && NR_CHANNELS > 5)
-        result[5] = 2;
-
-    return result;
-}
-
-
-static DelaysType
-delaysTestPattern(bool isBegin, bool isFused = false)
-{
-    DelaysType result(DelaysDims);
-    std::fill_n(result.data(), result.num_elements(), 0);
-
-    if (!isBegin && !isFused && NR_INPUTS > 22)
-        result[22] = 1e-6;
-
-    return result;
-}
-
-
-static void
-checkTransposeTestPattern(const CorrectedDataType& correctedData)
-{
-    for (int channel = 0; channel < NR_CHANNELS; channel ++)
-        for (int time = 0; time < NR_SAMPLES_PER_CHANNEL; time ++)
-            for (int input = 0; input < NR_INPUTS; input ++)
-                if (correctedData[channel][input][time] != (0.0f + 0.0if)) {
-                    cout << "channel = " << channel << ", time = " << time
-                         << ", input = " << input << ", value = "
-                         << correctedData[channel][input][time]
-                         << std::endl;
-                }
-}
-
-
-static CorrectedDataType
-testTranspose(FilteredDataType& filteredData)
-{
-    if (NR_INPUTS > 22 && NR_SAMPLES_PER_CHANNEL > 99 && NR_CHANNELS > 5) {
-        filteredData[22][99][5] = {2, 3};
-    }
-
-    BandPassCorrectionWeights bandPassCorrectionWeights(BandPassCorrectionWeightsDims);
-    if (bandpass_correction) {
-        bandPassCorrectionWeights = bandPassTestPattern();
-    }
-
-    auto correctedData = transpose(filteredData, bandPassCorrectionWeights);
-
-    if (delay_compensation) {
-        applyDelays(correctedData, delaysTestPattern(true), delaysTestPattern(false), 60e6);
-    }
-
-    if (output_check) checkTransposeTestPattern(correctedData);
-    return correctedData;
-}
-
-
-////// non-fused
-
 static CorrectedDataType
 nonfused
 ( const InputDataType& inputData
@@ -407,6 +485,7 @@ nonfused
 
     return result;
 }
+
 
 ////// fused
 
@@ -570,34 +649,6 @@ fused
 }
 
 
-static void
-checkFusedTestPattern(const CorrectedDataType& correctedData)
-{
-    for (unsigned input = 0; input < NR_INPUTS; input ++)
-        for (unsigned time = 0; time < NR_SAMPLES_PER_CHANNEL; time ++)
-            for (unsigned channel = 0; channel < NR_CHANNELS; channel ++)
-                if (correctedData[channel][input][time] != (0.0f + 0.0if)) {
-                    cout << "input = " << input << ", time = " << time
-                         << ", channel = " << channel << ": ("
-                         << correctedData[channel][input][time].real()
-                         << ", " << correctedData[channel][input][time].imag()
-                         << ')' << std::endl;
-                }
-}
-
-
-static void
-testFused()
-{
-    auto correctedData = fused(
-            inputTestPattern(true), filterWeightsTestPattern(true),
-            bandPassTestPattern(true),
-            delaysTestPattern(true, true), delaysTestPattern(false, true), 60e6);
-
-    if (output_check) checkFusedTestPattern(correctedData);
-}
-
-
 
 ////// correlator
 
@@ -633,33 +684,6 @@ correlate(const CorrectedDataType& correctedData)
     }
 
     return visibilities;
-}
-
-
-static void
-checkCorrelatorTestPattern(const VisibilitiesType& visibilities)
-{
-    for (unsigned channel = 0; channel < NR_CHANNELS; channel ++)
-        for (unsigned baseline = 0; baseline < NR_BASELINES; baseline ++)
-            if (visibilities[channel][baseline] != (0.0f + 0.0if)) {
-                cout << "channel = " << channel << ", baseline = " << baseline
-                     << ", visibility = " << visibilities[channel][baseline]
-                     << std::endl;
-            }
-}
-
-
-static void
-testCorrelator(CorrectedDataType& correctedData)
-{
-    if constexpr (NR_CHANNELS > 5 && NR_SAMPLES_PER_CHANNEL > 99 && NR_INPUTS > 19) {
-        correctedData[5][ 0][99] = {3,4};
-        correctedData[5][18][99] = {5,6};
-    }
-
-    auto visibilities = correlate(correctedData);
-
-    if (output_check) checkCorrelatorTestPattern(visibilities);
 }
 
 
